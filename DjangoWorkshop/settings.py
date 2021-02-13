2d86db5d1a9fd61a95fa94c12e909678d34106ea
Maciej <maciej.szulgacz@gmail.com>
2021-02-13 19:42:30 +0100

    Changed database

--- DjangoWorkshop/settings.py
+++ DjangoWorkshop/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'DjangoWorkshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': '127.0.0.1',
+        'NAME': 'workshopdjango_db',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
     }
 }
 
