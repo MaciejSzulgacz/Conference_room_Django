3d3ff979871c2cf612f8dda78488e55dc23b3432
Maciej <maciej.szulgacz@gmail.com>
2021-04-08 19:15:56 +0200

    requirements, .gitignore

--- DjangoWorkshop/settings.py
+++ DjangoWorkshop/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'DjangoWorkshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'HOST': '127.0.0.1',
-        'NAME': 'workshopdjango_db',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'postgres',
-        'PASSWORD': 'coderslab',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
